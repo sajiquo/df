01a946d51de6e9c34e65314e0cf27949d805df1b
sajiuqo <szmckw@gmail.com>
2024-04-08 10:53:53 +0900

    chore: initialize dotfiles

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://github.com/jesseduffield/lazygit#ubuntu
+install_lazygit() {
+    local -r LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+}
+
+install_lazygit
+echo \033[32m SETUP COMPLETED \033[m
